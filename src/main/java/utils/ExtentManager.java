e543015f8021298acb378a4b224f47768abe4922
Oluwafemi Adeoye <oadeoye@getcarbon.co>
2024-08-12 15:14:36 +0100

    Adding .xml file and mvn dependency.

---
+++ src/main/java/utils/ExtentManager.java
@@ -0,0 +1,32 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+    private static ExtentTest test;
+
+    public static ExtentReports getExtentReports() {
+        if (extent == null) {
+            ExtentSparkReporter sparkReporter = new ExtentSparkReporter("target/extent-report.html");
+            extent = new ExtentReports();
+            extent.attachReporter(sparkReporter);
+            extent.setSystemInfo("OS", System.getProperty("os.name"));
+            extent.setSystemInfo("Java Version", System.getProperty("java.version"));
+        }
+        return extent;
+    }
+
+    public static ExtentTest createTest(String testName) {
+        test = getExtentReports().createTest(testName);
+        return test;
+    }
+
+    public static void flushReports() {
+        if (extent != null) {
+            extent.flush();
+        }
+    }
+}
--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -1,5 +1,9 @@
 package basetest;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.google.common.io.Files;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
@@ -14,21 +18,28 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.EventReporter;
+import utils.ExtentManager;
 import utils.WindowManager;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.CookieManager;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-//    protected GoogleSearchPage searchPage;
-protected WebDriver driver;
+    protected WebDriver driver;
     protected HomePage homePage;
+    protected static ExtentReports extent;
+    protected static ExtentTest extentTest;
 
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
+
+        // Initialize ExtentReports
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("target/extent-report.html");
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+        extent.setSystemInfo("OS", System.getProperty("os.name"));
+        extent.setSystemInfo("Java Version", System.getProperty("java.version"));
 
         // Initialize the WebDriver
         WebDriver originalDriver = new ChromeDriver();
@@ -42,35 +53,51 @@ protected WebDriver driver;
 
     @BeforeMethod
     public void goHome() {
-//        driver.manage().deleteAllCookies(); // Deletes all the cookies
         driver.get("https://dashboard.helloagain.at/#/login");
-        driver.manage().window().maximize(); // This maximize
-//        driver.manage().window().setSize(new Dimension(375,812)); // Allows you to use a phone dimension from devtools emulating a phone resolution. The dimension set is from the device selected.
+        driver.manage().window().maximize();
     }
 
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
+        // Flush the ExtentReports
+        if (extent != null) {
+            extent.flush();
+        }
+    }
+
+    // Screenshot capture method
+    public String captureScreenshot(WebDriver driver, String screenshotName) {
+        String screenshotPath = "resources/screenshots/" + screenshotName + ".png";
+        try {
+            TakesScreenshot camera = (TakesScreenshot) driver;
+            File source = camera.getScreenshotAs(OutputType.FILE);
+            File destination = new File(screenshotPath);
+//            Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+            Files.copy(source.toPath().toFile(), destination.toPath().toFile());
+        } catch (Exception e) {
+            System.out.println("Exception while taking screenshot: " + e.getMessage());
+        }
+        return screenshotPath;
     }
 
     @AfterMethod
-    public void recordFailure(ITestResult result) {
-        if (ITestResult.FAILURE == result.getStatus()) {
-            TakesScreenshot camera = (TakesScreenshot) driver;
-            File screenshot = camera.getScreenshotAs(OutputType.FILE);
-            try {
-                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public void tearDownTest(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            String screenshotPath = captureScreenshot(driver, result.getName());
+            extentTest.fail("Test failed: " + result.getThrowable(),
+                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            extentTest.skip("Test skipped: " + result.getThrowable());
+        } else {
+            extentTest.pass("Test passed");
         }
+        ExtentManager.flushReports();
     }
 
     public WindowManager getWindowManager() {
         return new WindowManager(driver);
-    }
-
-    public CookieManager getCookieManager() {
-        return new CookieManager();
     }
 }
